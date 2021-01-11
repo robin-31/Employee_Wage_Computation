bf21dd80489d4e4c385a01b923036accce811569
“robin-31” <rm.robinmanna01@gmail.com>
2021-01-11 20:06:46 +0530

    Added: add part time employee and there wage

--- employeeWageComputation.sh
+++ employeeWageComputation.sh
@@ -1,13 +1,19 @@
 #!/bin/bash -x
 #"Welcome to Employee Wage Computatio Program on master branch"
-isPresent=1
-WAGE_PER_HOUR=20
-FULL_DAY_HOUR=8
-tempval=$((RANDOM%2))
-if [ $isPresent -eq $tempval ]
+isPartTime=1
+isFullTime=2
+empRate_PER_HOUR=20
+tempval=$((RANDOM%3))
+if [ $isFullTime -eq $tempval ]
 then
-	daily_wage=$[ $WAGE_PER_HOUR * $FULL_DAY_HOUR ]
 	echo "employee is present"
+	workingHrs=8
+elif [ $isPartTime -eq $tempval ]
+	then
+		echo "employee is partime"
+		workingHrs=4
 else
 	echo "employee is absent"
 fi
+dailyWage=$(($workingHrs * $empRate_PER_HOUR))
+echo $dailyWage
