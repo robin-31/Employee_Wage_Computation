ae61381b0992b355098aa53b3ff918d942cbda40
“robin-31” <rm.robinmanna01@gmail.com>
2021-01-11 19:34:46 +0530

    Added: check whether employee is present or absent

--- employeeWageComputation.sh
+++ employeeWageComputation.sh
@@ -1,3 +1,10 @@
 #!/bin/bash -x
-
-echo "Welcome to Employee Wage Computatio Program on master branch"
+#"Welcome to Employee Wage Computatio Program on master branch"
+isPresent=1
+tempval=$((RANDOM%2))
+if [ $isPresent -eq $tempval ]
+then
+	echo "employee is present"
+else
+	echo "employee is absent"
+fi
