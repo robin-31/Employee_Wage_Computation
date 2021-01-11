ae0986d138f7249f66de9490b469283de9154575
“robin-31” <rm.robinmanna01@gmail.com>
2021-01-11 19:47:38 +0530

    Added: calculation of employee daily wages

--- employeeWageComputation.sh
+++ employeeWageComputation.sh
@@ -1,9 +1,12 @@
 #!/bin/bash -x
 #"Welcome to Employee Wage Computatio Program on master branch"
 isPresent=1
+WAGE_PER_HOUR=20
+FULL_DAY_HOUR=8
 tempval=$((RANDOM%2))
 if [ $isPresent -eq $tempval ]
 then
+	daily_wage=$[ $WAGE_PER_HOUR * $FULL_DAY_HOUR ]
 	echo "employee is present"
 else
 	echo "employee is absent"
