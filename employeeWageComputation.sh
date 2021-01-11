7a35f8ee7ed19eded819c678bec3ed5f286b75d2
“robin-31” <rm.robinmanna01@gmail.com>
2021-01-11 22:42:00 +0530

    ADDED: calculation of salary for whole month

--- employeeWageComputation.sh
+++ employeeWageComputation.sh
@@ -3,20 +3,26 @@
 isPartTime=1
 isFullTime=2
 empRate_PER_HOUR=20
-empType=$((RANDOM%3))
-case $empType in
-	$isFullTime)
-		echo "employee is present"
-		workingHrs=8
-		;;
-	$isPartTime)
-		echo "employee is partime"
-		workingHrs=4
-		;;
-	*)
-		echo "employee is absent"
-		workingHrs=0
-		;;
-esac
-dailyWage=$(($workingHrs * $empRate_PER_HOUR))
-echo $dailyWage
+totalSalary=0
+workingDays=30
+for (( i=1; day<=$workingDays; day++ ))
+do
+	empType=$((RANDOM%3))
+	case $empType in
+		$isFullTime)
+			echo "employee is present"
+			workingHrs=8
+			;;
+		$isPartTime)
+			echo "employee is partime"
+			workingHrs=4
+			;;
+		*)
+			echo "employee is absent"
+			workingHrs=0
+			;;
+	esac
+	dailyWage=$(($workingHrs * $empRate_PER_HOUR))
+	totalSalary=$(( $dailyWage + totalSalary ))
+done
+echo $totalSalary
