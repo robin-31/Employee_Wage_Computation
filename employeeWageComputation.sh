d7d9689149bd8bbe9fe8da0d95678c264f3636bd
“robin-31” <rm.robinmanna01@gmail.com>
2021-01-11 23:27:13 +0530

    ADDED: new condition added on working hours and working days

--- employeeWageComputation.sh
+++ employeeWageComputation.sh
@@ -1,19 +1,25 @@
 #!/bin/bash -x
 #"Welcome to Employee Wage Computatio Program on master branch"
-isPartTime=1
-isFullTime=2
-empRate_PER_HOUR=20
+# CONSTANT 
+IS_PART_TIME=1
+IS_FULL_TIME=2
+EMP_RATE_PER_HRS=20
+MAX_WORKING_HRS_IN_MONTH=100
+NUM_WORKING_DAYS=20 
+# VARIABLES
 totalSalary=0
-workingDays=30
-for (( i=1; day<=$workingDays; day++ ))
+totalWorkingDays=0
+totalWorkingHrs=0
+while [[ $totalWorkingHrs -lt $MAX_WORKING_HRS_IN_MONTH && $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
 do
+	((totalWorkingDays++))
 	empType=$((RANDOM%3))
 	case $empType in
-		$isFullTime)
+		$IS_FULL_TIME)
 			echo "employee is present"
 			workingHrs=8
 			;;
-		$isPartTime)
+		$IS_PART_TIME)
 			echo "employee is partime"
 			workingHrs=4
 			;;
@@ -22,7 +28,6 @@ do
 			workingHrs=0
 			;;
 	esac
-	dailyWage=$(($workingHrs * $empRate_PER_HOUR))
-	totalSalary=$(( $dailyWage + totalSalary ))
+	totalWorkingHrs=$(($workingHrs + $totalWorkingHrs))
 done
-echo $totalSalary
+totalSalary=$(($totalWorkingHrs * $EMP_RATE_PER_HRS))
