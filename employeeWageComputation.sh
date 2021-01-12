904c8853e5d515f73ba0d69687fa90a75a7f46a8
“robin-31” <rm.robinmanna01@gmail.com>
2021-01-12 12:53:55 +0530

    ADDED: everyday employee wages stored in array

--- employeeWageComputation.sh
+++ employeeWageComputation.sh
@@ -4,8 +4,8 @@
 IS_PART_TIME=1
 IS_FULL_TIME=2
 EMP_RATE_PER_HRS=20
-MAX_WORKING_HRS_IN_MONTH=100
-NUM_WORKING_DAYS=20 
+MAX_WORKING_HRS_IN_MONTH=50
+NUM_WORKING_DAYS=20
 # VARIABLES
 #totalSalary=0
 totalWorkingDays=0
@@ -25,11 +25,18 @@ function getworkingHrs() {
         esac
 	echo $workingHrs
 }
+function getDailyWage() {
+	local workingHrs=$1
+	wage=$(($workingHrs*$EMP_RATE_PER_HRS))
+	echo $wage
+}
 while [[ $totalWorkingHrs -lt $MAX_WORKING_HRS_IN_MONTH && $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
 do
 	((totalWorkingDays++))
 	empType=$(( RANDOM%3 ))
 	workingHrs="$( getworkingHrs $empType )"
 	totalWorkingHrs=$(( $totalWorkingHrs + $workingHrs ))
+	dailyWage[$totalWorkingDays]=$( getDailyWage $workingHrs )
 done
 totalSalary=$(( $totalWorkingHrs * $EMP_RATE_PER_HRS ))
+echo ${dailyWage[@]}
