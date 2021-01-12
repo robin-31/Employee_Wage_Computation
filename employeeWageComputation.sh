7a0be837dfe1092730f01acac0ee11cb5b71671b
“robin-31” <rm.robinmanna01@gmail.com>
2021-01-12 13:31:43 +0530

    ADDED: employee daily wage stored in dict along with day

--- employeeWageComputation.sh
+++ employeeWageComputation.sh
@@ -4,8 +4,9 @@
 IS_PART_TIME=1
 IS_FULL_TIME=2
 EMP_RATE_PER_HRS=20
-MAX_WORKING_HRS_IN_MONTH=50
+MAX_WORKING_HRS_IN_MONTH=100
 NUM_WORKING_DAYS=20
+declare -A dailyWage
 # VARIABLES
 #totalSalary=0
 totalWorkingDays=0
@@ -36,7 +37,9 @@ do
 	empType=$(( RANDOM%3 ))
 	workingHrs="$( getworkingHrs $empType )"
 	totalWorkingHrs=$(( $totalWorkingHrs + $workingHrs ))
-	dailyWage[$totalWorkingDays]=$( getDailyWage $workingHrs )
+	dailyWage[ "DAY: $totalWorkingDays"]=$( getDailyWage $workingHrs )
 done
 totalSalary=$(( $totalWorkingHrs * $EMP_RATE_PER_HRS ))
+echo ${!dailyWage[@]}
 echo ${dailyWage[@]}
+
