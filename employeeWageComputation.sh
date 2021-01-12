f0ec48fc662137a202d61a776022b9faebb04c4b
“robin-31” <rm.robinmanna01@gmail.com>
2021-01-12 11:46:36 +0530

    REFACTOR: add new function to check employee attendace

--- employeeWageComputation.sh
+++ employeeWageComputation.sh
@@ -7,27 +7,29 @@ EMP_RATE_PER_HRS=20
 MAX_WORKING_HRS_IN_MONTH=100
 NUM_WORKING_DAYS=20 
 # VARIABLES
-totalSalary=0
+#totalSalary=0
 totalWorkingDays=0
 totalWorkingHrs=0
+function getworkingHrs() {
+	local empType=$1
+	case $empType in
+                $IS_FULL_TIME)
+                        workingHrs=8
+                        ;;
+                $IS_PART_TIME)
+                        workingHrs=4
+                        ;;
+                *)
+                        workingHrs=0
+                        ;;
+        esac
+	echo $workingHrs
+}
 while [[ $totalWorkingHrs -lt $MAX_WORKING_HRS_IN_MONTH && $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
 do
 	((totalWorkingDays++))
-	empType=$((RANDOM%3))
-	case $empType in
-		$IS_FULL_TIME)
-			echo "employee is present"
-			workingHrs=8
-			;;
-		$IS_PART_TIME)
-			echo "employee is partime"
-			workingHrs=4
-			;;
-		*)
-			echo "employee is absent"
-			workingHrs=0
-			;;
-	esac
-	totalWorkingHrs=$(($workingHrs + $totalWorkingHrs))
+	empType=$(( RANDOM%3 ))
+	workingHrs="$( getworkingHrs $empType )"
+	totalWorkingHrs=$(( $totalWorkingHrs + $workingHrs ))
 done
-totalSalary=$(($totalWorkingHrs * $EMP_RATE_PER_HRS))
+totalSalary=$(( $totalWorkingHrs * $EMP_RATE_PER_HRS ))
